0b22718d8854b0e1376849bc8b9e934aa59096f0
trkha16 <tbkha1609@gmail.com>
2022-11-09 14:08:42 +0700

    wip move animal

--- CANIMAL.cpp
+++ CANIMAL.cpp
@@ -14,29 +14,42 @@ CDINAUSOR::CDINAUSOR(int numLane, int direction, int left, int top)
 
 bool CDINAUSOR::isInLane(int x)
 {
-	return (x > _borderLeft && x < _borderRight-1);
+	return (x > _borderLeft && x < _borderRight - 1);
 }
 
 void CDINAUSOR::renderDinausor()
 {
 	Common::setConsoleColor(BRIGHT_WHITE, BLACK);
-	mX = 8;
-	int curX = mX + 2, curY = mY + 1;
-	for (int cnt = 0; cnt < _numDino; cnt++) {
-		for (int i = 0; i < 5; i++) {
-			int befX = curX;
-			for (int j = 0; j < 16; j++) {
-				Common::setConsoleColor(BRIGHT_WHITE, BLACK);
-				Common::gotoXY(curX, curY);
-				if (!isInLane(curX))
-					curX = _left + 2;
-				std::cout << data[i][j];
-				++curX;
+	mX = 30;
+	int curX = 0, curY = 0, prevX;
+	while (true) {
+		mX += 2;
+		curX = mX + 2;
+		curY = mY + 1;
+		Sleep(100);
+		for (int cnt = 0; cnt < _numDino; cnt++) {
+			for (int i = 0; i < 5; i++) {
+				prevX = curX;
+				for (int j = 0; j < 16; j++) {
+					Common::setConsoleColor(BRIGHT_WHITE, BLACK);
+					Common::gotoXY(curX, curY);
+					if (!isInLane(curX))
+						curX = _left + 2;
+					std::cout << data[i][j];
+					++curX;
+				}
+				curX = prevX;
+				++curY;
 			}
-			curX = befX;
-			++curY;
+			//Sleep(200);
+			for (int i = 0; i < 5; i++) {
+				Common::gotoXY(prevX - 1, curY - i - 1);
+				putchar(32);
+				Common::gotoXY(prevX - 2, curY - i - 1);
+				putchar(32);
+			}
+			curY -= 5;
+			curX += 16 + _spaceDino;
 		}
-		curY -= 5;
-		curX += 16 + _spaceDino;
 	}
 }
