3883132fd81a86ee4956a559fad86aae105d5af5
trkha16 <tbkha1609@gmail.com>
2022-11-11 14:39:15 +0700

    reverse truck

--- CVEHICLE.cpp
+++ CVEHICLE.cpp
@@ -87,39 +87,39 @@ void CTRUCK::renderTruck()
 	vector<int> curY;
 	int sizeX = 17;
 	int sizeY = 4;
-	int startPos = _left + 3;
+	int startPos = _borderRight - 3;
 	curX.push_back(startPos);
 	curY.push_back(mY + 1);
 	for (int i = 1; i < _numTruck; i++) {
 		int tmp = curX[i - 1];
-		curX.push_back(tmp + 16 + _spaceTruck);
+		curX.push_back(tmp - 17 - _spaceTruck);
 		curY.push_back(mY + 1);
 	}
 	int prevX;
 	while (true) {
 		Sleep(100);
-		for (int cnt = 0; cnt < _numTruck; cnt++) {
+		for (int cnt = _numTruck - 1; cnt >= 0; cnt--) {
 			prevX = curX[cnt];
 			for (int i = 0; i < sizeY; i++) {
-				for (int j = 0; j < sizeX; j++) {
+				for (int j = sizeX - 1; j >= 0; j--) {
 					Common::setConsoleColor(BRIGHT_WHITE, BLACK);
 					if (!isInLane(curX[cnt]))
 						curX[cnt] = startPos;
 					Common::gotoXY(curX[cnt], curY[cnt]);
 					std::cout << data[i][j];
-					curX[cnt] = curX[cnt] + 1;
+					curX[cnt] = curX[cnt] - 1;
 				}
 				curX[cnt] = prevX;
 				curY[cnt] = curY[cnt] + 1;
 			}
 			for (int i = 0; i < sizeY; i++) {
-				Common::gotoXY(prevX - 1, (int)curY[cnt] - i - 1);
+				Common::gotoXY(prevX + 1, (int)curY[cnt] - i - 1);
 				putchar(32);
 				Common::gotoXY(_borderRight - 2, (int)curY[cnt] - i - 1);
 				putchar(32);
 			}
-			curX[cnt] = prevX + 1;
-			if (!isInLane(prevX + 1))
+			curX[cnt] = prevX - 1;
+			if (!isInLane(prevX - 1))
 				curX[cnt] = startPos;
 			curY[cnt] = mY + 1;
 		}
