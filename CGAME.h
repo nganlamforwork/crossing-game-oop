4367e712deb746c124a6ee812e926b00653aabb8
Tran Binh Kha <94106543+trkha16@users.noreply.github.com>
2022-11-13 23:10:09 +0700

    people move

--- CGAME.h
+++ CGAME.h
@@ -3,17 +3,19 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
+#include <thread>
 #include <vector>
 #include "Common.h"
 #include "CANIMAL.h"
+#include "CPEOPLE.h"
 #include "CVEHICLE.h"
-#include <thread>
 
 class CGAME {
 	CDINAUSOR* kl;
 	CCAR* xe;
 	CBIRD* bird;
 	CTRUCK* truck;
+	CPEOPLE* people;
 	int _left = 1, _top = 1;
 	int NUM_LANE = 6;
 public:
